e335d266dcdf15383837ff15a2b61afcd04f851d
Danilo César <danilocesar@outlook.com>
2022-03-19 20:44:19 -0300

    chore: add JSDoc comments on scripts, and metatags

--- src/js/calendar.js
+++ src/js/calendar.js
@@ -1,8 +1,11 @@
+// Get calendar events from the `app()` instance
 events = app().tasks;
 
+// Load this block whenever DOM Content is fully loaded
 document.addEventListener('DOMContentLoaded', function () {
     var calendarEl = document.getElementById('calendar');
     
+    // Configure and render FullCalendar
     var calendar = new FullCalendar.Calendar(calendarEl, {
         events: events,
 
