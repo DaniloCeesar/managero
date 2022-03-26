9bbc1e0e6dfa9e1400fb4b514f554e0347cdda1d
Danilo César <danilocesar@outlook.com>
2022-03-26 18:37:59 -0300

    style: fix New Folder button width in the sidebar

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -314,7 +314,7 @@ const lists = ref([
 
             <li>
               <button
-                class="flex items-center p-2 text-sm font-medium text-gray-500 rounded-lg hover:bg-gray-100 hover:text-indigo-500 group"
+                class="flex items-center w-full p-2 text-sm font-medium text-gray-500 rounded-lg hover:bg-gray-100 hover:text-indigo-500 group"
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -330,7 +330,7 @@ const lists = ref([
                   <line x1="12" y1="5" x2="12" y2="19" />
                   <line x1="5" y1="12" x2="19" y2="12" />
                 </svg>
-                <span class="flex-1 ml-4 whitespace-nowrap">New folder</span>
+                <span class="flex-1 ml-4 text-left whitespace-nowrap">New folder</span>
               </button>
             </li>
           </ul>
